77207dad19423aa2c6ddb3da0a71cfedc6a69a69
musayann <musayann@octangrp.com>
2021-02-12 08:37:49 +0200

    Add netlify cms data importer

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+
     if (!isServer) {
       config.node = { fs: "empty", module: "empty" };
     }
