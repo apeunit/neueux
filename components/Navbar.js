a6d8cd76687341f24e06c8f0585f3add1b73b9c5
karemarsy <karemarsy.com>
2021-02-20 10:14:39 +0200

    create the home page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 
 const Navbar = () => (
 <nav className="">
     <div className="w-11/12 mx-auto border-b-2 border-gray-200">
-        <div className="flex my-8 ">
+        <div className="flex my-8 text-sm">
             <ul className="w-2/6">
-                <li><a href="">Neueux logo</a></li>
+                <li><a href=""><Image src="/public/img/logo.svg" width={10} height={10}/></a></li>
             </ul>
 
-            <ul className="w-2/6 text-center">
-                <li className="inline-block mx-7"><a href="">Screens</a></li>
+            <ul className="w-2/6 text-center ">
+                <li className="inline-block mx-7 "><a href="">Screens</a></li>
                 <li className="inline-block mx-7"><a href="">Articles</a></li>
                 <li className="inline-block mx-7"><a href="">Glossary</a></li>
             </ul>
 
-            <ul className="w-2/6 text-right text-accent-1 text-red">
-                <li><a href=""><button>Contribute</button></a></li>
+            <ul className="w-2/6 text-right">
+                <li className=" font-bold text-red-600"><a href="">Contribute</a></li>
             </ul>
         </div>
         
