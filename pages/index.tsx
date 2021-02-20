224181c2fa1b6e96778f684f3998487897980ceb
karemarsy <karemarsy.com>
2021-02-20 10:14:39 +0200

    create the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,6 @@ import Navbar from "components/Navbar";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <Navbar/>
-    {/* <h1 className="red">Hello Next.js 👋</h1>
-    <p className="text-lg font-semibold">
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-    <Button className="text-red-900 my-3">Hello Twin Macro</Button> */}
   </Layout>
 );
 
