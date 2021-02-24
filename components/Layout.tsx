c1b3478ed4e9ba67b8a9bd5f40ddb0385398820f
karemarsy <karemarsy.com>
2021-02-24 10:24:20 +0200

    Add images

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,28 +1,29 @@
-import React, { ReactNode } from "react";
-import Head from "next/head";
-import Navbar from "components/Navbar";
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
 
 type Props = {
-  children?: ReactNode;
-  title?: string;
-};
+  children?: ReactNode
+  title?: string
+}
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div className="xl:m-5">
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+  <div className="m-5">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <Navbar />
+      <nav>
+        header
+      </nav>
     </header>
     {children}
-    {/* <footer>
+    <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
-    </footer> */}
+    </footer>
   </div>
-);
+)
 
-export default Layout;
+export default Layout
--- components/card/Screen.js
+++
@@ -1,9 +0,0 @@
-import { type } from "os";
-
-const ScreenCard = ({ url, style }) => (
-  <div className={style === "desktop" ? "desktop-card mb-12 xl:mb-0" : "card w-5/12 mb-12 xl:mb-0"}>
-    <img src={url} />
-  </div>
-);
-
-export default ScreenCard;
