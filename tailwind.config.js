0eca060eaabdfa7fd15dfc2d0a7d6f108de28cbb
musayann <musayann@octangrp.com>
2021-02-25 18:25:17 +0200

    Refactor navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
 
+      accent: '#FF3A02',
       black: colors.black,
       white: colors.white,
       gray: colors.coolGray,
