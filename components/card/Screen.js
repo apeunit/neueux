a35e1770bd2a4b0dc727597d6b38d86bdea32ad4
musayann <musayann@octangrp.com>
2021-02-25 18:26:44 +0200

    Fix screen border

--- components/card/Screen.js
+++ components/card/Screen.js
@@ -1,7 +1,7 @@
 import { type } from "os";
 
 const ScreenCard = ({ url, style }) => (
-  <div className={[style === "desktop" ? "desktop-screen-card" : "mobile-screen-card ", 'mb-12 xl:mb-0 rounded-lg filter-drop-shadow'].join(' ')}>
+  <div className={[style === "desktop" ? "desktop-screen-card" : "mobile-screen-card ", 'mb-12 xl:mb-0 rounded-lg filter-drop-shadow overflow-hidden'].join(' ')}>
     <img src={url} className="w-full"/>
   </div>
 );
