f1d169061890290c321e90023f3cb9ed73872bbf
karemarsy <karemarsy.com>
2021-02-25 14:35:51 +0200

    Implement screen page

--- components/card/App.js
+++ components/card/App.js
@@ -1,28 +1,11 @@
 import Screen from "components/card/Screen.js";
 import { join } from "path";
 
-const AppCard = ({ app }) => (
+const AppCard = ({ screen }) => (
   <div>
-    <div className="flex mt-10">
-      <div>
-        <img className="app-logo" src={app.logo} />
-      </div>
-      <div className="mx-3 mt-2 text-sm font-bold">
-        <h3 className=""> {app.name} </h3>
-      </div>
-    </div>
-
-    <div
-      className={[
-        "mt-5 flex flex-wrap",
-        app.screens.length < 6 && app.type === "mobile"
-          ? "space-x-5"
-          : "justify-between",
-      ].join(" ")}
-    >
-      {app.screens.map((screen) => {
-        return <Screen url={screen.image} style={app.type} />;
-      })}
+  
+    <div className="mt-5">
+        <Screen url={screen.image} />;
     </div>
   </div>
 );
---
+++ pages/screen.tsx
@@ -0,0 +1,74 @@
+// import Layout from "../components/Layout";
+import React from "react";
+// import AppCard from "../components/card/App";
+
+const ScreenPage = () => {
+  //   const apps = {
+  //     screens: { image: "/img/2.png" },
+  //   };
+
+  return (
+    <main>
+      {/* <AppCard app={apps} />; */}
+      <div className="flex">
+        <div className="mt-16 mb-16 w-full flex justify-center ">
+          <img src="./img/2.png" className="card1" />
+        </div>
+
+        <div className="h-screen w-96 bg-gray-50  mr-auto">
+          <div className="absolute right-4 top-4">
+            <div className="bg-gray-50 rounded-full border-2 border-solid border-gray-200 ml-2">
+              <a href="">
+                <img src="/img/close.png" className="m-3.5" />
+              </a>
+            </div>
+          </div>
+
+          <div className="text-center mt-56">
+            <div className="flex justify-center">
+              <img src="/img/bitpay.png" />
+            </div>
+            <p className="text-sm mt-4 font-extrabold leading-6 tracking-tighter">
+              BitPay - Buy Crypto
+            </p>
+            <span className="text-sm text-gray-500 mt-1 tracking-tighter leading-6">
+              A Secure Bitcoin Wallet
+            </span>
+          </div>
+          <div className="border-b-2 border-solid mx-8 mt-5 mb-6"></div>
+          <div className="flex justify-center flex-wrap mx-6">
+            <div className="mb-3">
+              <a className="bg-gray-50 px-4 py-1.5 text-xs rounded-2xl border-2 border-solid border-gray-200 font-bold text-gray-500">
+                Onboarding
+              </a>
+            </div>
+            <div className="mx-1 mb-3">
+              <a className="bg-gray-50 px-4 py-1.5 text-xs rounded-2xl border-2 border-solid border-gray-200 font-bold text-gray-500">
+                Balance
+              </a>
+            </div>
+            <div className="rounded-full mb-3">
+              <a className="bg-gray-50 px-4 py-1.5 text-xs rounded-2xl border-2 border-solid border-gray-200 font-bold text-gray-500">
+                Home screen
+              </a>
+            </div>
+          </div>
+          <div className="flex absolute right-4 bottom-4">
+            <div className="bg-gray-50 rounded-full border-2 border-solid border-gray-200 ">
+              <a href="">
+                <img src="/img/right-arrow.png" className="m-3.5" />
+              </a>
+            </div>
+            <div className="bg-gray-50 rounded-full border-2 border-solid border-gray-200 ml-2">
+              <a href="">
+                <img src="/img/left-arrow.png" className="m-3.5" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ScreenPage;
