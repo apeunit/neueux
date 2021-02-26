5af5f7a4cf2572293ae68fffe9fb0542a935bac3
karemarsy <karemarsy@gmail.com>
2021-02-26 08:55:55 +0200

    Fix css issues

--- components/card/App.js
+++ components/card/App.js
@@ -12,14 +12,7 @@ const AppCard = ({ app }) => (
       </div>
     </div>
 
-    <div
-      className={[
-        "mt-5 flex flex-wrap",
-        app.screens.length < 6 && app.type === "mobile"
-          ? "space-x-5"
-          : "justify-between",
-      ].join(" ")}
-    >
+    <div className="mt-5 flex space-x-5">
       {app.screens.map((screen) => {
         return <Screen url={screen.image} style={app.type} />;
       })}
