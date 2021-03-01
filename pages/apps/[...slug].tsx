1ff79a75f90d8b13db39c616d2829bfe51feee54
musayann <musayann@octangrp.com>
2021-03-01 19:02:27 +0200

    Add toggle filter

--- pages/apps/[...slug].tsx
+++ pages/apps/[...slug].tsx
@@ -1,27 +1,35 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import Layout from "../../components/Layout";
-import React from "react";
+import React, { useState } from "react";
 import Screen from "../../components/card/Screen.js";
 import HeaderView from "../../components/sections/HeaderView";
 import ScreenView from "../../components/sections/ScreenView";
 import { getAppContent, listAllAppContent } from "../../lib/app";
 import { getAllAppScreenContent, getScreenContent } from "../../lib/screen";
 import Link from "next/link";
+import Filter from "../../components/sections/Filter";
+import { listTags } from "../../lib/tags";
+import { listUserflows } from "../../lib/userflows";
 
-const App = ({ app, screens, screen, screenNavigation }) => {
+const App = ({ app, screens, screen, screenNavigation, tags, userflows }) => {
+  const [showFilter, setShowFilter] = useState(false);
+
   if (screen) {
     return (
-      <ScreenView
-        screen={screen}
-        app={app}
-        navigation={screenNavigation}
-      />
+      <ScreenView screen={screen} app={app} navigation={screenNavigation} />
     );
   }
   return (
     <Layout title={app.name}>
       <main className="w-11/12 mx-auto">
-        <HeaderView app={app} />
+        {showFilter && (
+          <Filter
+            tags={tags}
+            userflows={userflows}
+            onClose={() => setShowFilter(false)}
+          />
+        )}
+        <HeaderView app={app} onOpenFilter={() => setShowFilter(true)} />
         <div
           className={[
             "mt-5 grid  gap-5",
@@ -43,13 +51,15 @@ const App = ({ app, screens, screen, screenNavigation }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async ({
-  params,
-}) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const app = getAppContent(params.slug[0]);
   const screens = getAllAppScreenContent(params.slug[0]);
   const screen = params.slug[2] ? getScreenContent(params.slug[2]) : null;
   const screenNavigation = { prev: null, next: null };
+
+  const tags = listTags();
+  const userflows = listUserflows();
+
 
   if (screen) {
     const screenIndex = screens.map((s) => s.slug).indexOf(screen.slug);
@@ -69,6 +79,8 @@ export const getStaticProps: GetStaticProps = async ({
       screens,
       screen,
       screenNavigation,
+      tags,
+      userflows
     },
   };
 };
@@ -83,7 +95,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         slug: [app.slug],
       },
     });
-    screens.forEach((screen, i) => {
+    screens.forEach((screen) => {
       paths.push({
         params: {
           slug: [app.slug, "screen", screen.slug],
