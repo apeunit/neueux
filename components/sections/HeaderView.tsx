94120653660915dad106653bac6b5e6f9543e9b6
karemarsy <karemarsy@gmail.com>
2021-03-02 16:00:23 +0200

    Fix the spacing issue

--- components/sections/HeaderView.tsx
+++ components/sections/HeaderView.tsx
@@ -4,14 +4,17 @@ import Link from "next/link";
 const HeaderView = ({ app, onOpenFilter }) => (
   <div className="">
     <div className="mt-5">
+      <div className="pt-1">
       <Link href="/">
         <a className="back-button cursor-pointer rounded-full bg-gray-100 px-5 py-3 font-bold text-xs">
           <img src="/img/arrow.png" className="inline-block mr-2" />
           Back
         </a>
       </Link>
+      </div>
+      
     </div>
-    <div className="text-center mt-7">
+    <div className="text-center mt-8">
       <div className="flex justify-center">
         <img src={`/${app.icon}`} className="w-10" />
       </div>
