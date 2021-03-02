27a9cae0922ca2c6f1882d69a1b511a972884e89
musayann <musayann@octangrp.com>
2021-03-02 22:06:04 +0200

    Add filter badge

--- components/card/App.js
+++ components/card/App.js
@@ -23,14 +23,16 @@ const AppCard = ({ app }) => (
       className={[
         "mt-5 grid  gap-5",
         app.device === "mobile" ? "grid-cols-6" : "grid-cols-2",
-      ].join(' ')}
+      ].join(" ")}
     >
       {app.screens.map((screen) => {
         return (
-        <Link href={`/apps/${app.slug}`}>
-          <a><Screen url={screen.image} style={app.type} /></a>
+          <Link key={`screen-card-${screen.slug}`} href={`/apps/${app.slug}`}>
+            <a>
+              <Screen url={screen.image} style={app.type} />
+            </a>
           </Link>
-          )
+        );
       })}
     </div>
   </div>
