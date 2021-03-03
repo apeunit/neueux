569cb205fbaef5985c422d7284f8344c5842b0ec
karemarsy <karemarsy@gmail.com>
2021-03-03 15:16:52 +0200

    Fix the responsiveness

--- components/card/Filter.tsx
+++ components/card/Filter.tsx
@@ -13,13 +13,13 @@ const ListItem = ({ text, onSelect }) => (
 const FilterCard = ({ tags, userflows, selectedList, onClose, onSelect, onRemove, index}) => {
   console.log(selectedList);
   return (
-    <div className="z-50 fixed h-screen top-0 py-4 pr-4 right-0 text-left text-gray-700 ">
-      <div className="bg-white rounded-2xl w-96  ars h-full flex flex-col overflow-hidden">
+    <div className="z-50 fixed h-screen top-0 py-4 xl:w-auto w-full px-4 xl:px-0 right-0 xl:pr-4 text-left text-gray-700">
+      <div className="bg-white rounded-2xl xl:w-96 mx-auto xl:mx-0 ars h-full flex flex-col overflow-hidden">
         <div className="pb-3 font-extrabold pt-7 flex bg-white w-full px-8 justify-between">
           <h1 className="text-xl">Filters</h1>
           <div
             onClick={onClose}
-            className="rounded-full cursor-pointer   border-2 border-solid border-gray-200 "
+            className="rounded-full cursor-pointer  border-2 border-solid border-gray-200 "
           >
             <img src="/img/close.png" className="m-3" />
           </div>
@@ -65,7 +65,7 @@ const FilterCard = ({ tags, userflows, selectedList, onClose, onSelect, onRemove
           </div>
         </div>
 
-        <div className="text-xs bottom-10 fixed px-8">
+        <div className="text-xs xl:bottom-10 bottom-2 xl:fixed relative flex justify-center xl:px-8">
           <span
             onClick={onClose}
             className="bg-gray-900 text-white text-xs px-36 leading-4 py-3 font-bold transition-250ms rounded-full"
--- pages/apps/[...slug].tsx
+++ pages/apps/[...slug].tsx
@@ -25,13 +25,8 @@ const App = ({ app, screens, screen, screenNavigation, tags, userflows }) => {
         <Filter tags={tags} userflows={userflows} />
         <div
           className={[
-<<<<<<< HEAD
             "mt-5 grid  gap-5",
             app.device === "mobile" ? "xl:grid-cols-6 grid-cols-2" : "grid-cols-2",
-=======
-            "mt-7 grid  gap-5",
-            app.device === "mobile" ? "grid-cols-6" : "grid-cols-2",
->>>>>>> main
           ].join(" ")}
         >
           {screens.map((screen) => {
