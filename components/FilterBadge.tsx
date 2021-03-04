3bc05aa05deb60ad779a6bf2980bba5f508d029f
musayann <musayann@octangrp.com>
2021-03-04 09:02:28 +0200

    remove filter badge spacing

--- components/FilterBadge.tsx
+++ components/FilterBadge.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FilterBadge = ({ text, onSelect }) => (
   <div
     onClick={onSelect}
-    className="cursor-pointer rounded-full bg-gray-100 px-5 py-3 font-bold text-xs capitalize mr-2 mb-9"
+    className="cursor-pointer rounded-full bg-gray-100 px-5 py-3 font-bold text-xs capitalize mr-2 mb-2"
   >
     {text}
     <img src="/img/remove.svg" className="inline-block ml-3" />
