c9d5f6fb84ccf089201fc618d22d6015f05e7ac8
karemarsy <karemarsy@gmail.com>
2021-03-17 09:49:35 +0200

    Fix the error in the navbar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,20 +2,6 @@ import React from "react";
 import Link from "next/link";
 
 const MobileNavbar = () => {
-  const items = [
-    {
-      link: "/",
-      label: "Screens",
-    },
-    {
-      link: "/",
-      label: "Articles",
-    },
-    {
-      link: "/",
-      label: "Glossary",
-    },
-  ];
   return (
     <nav className="lg:hidden">
       <div className="w-11/12 mx-auto border-b border-gray-200">
@@ -32,7 +18,7 @@ const MobileNavbar = () => {
 
           <ul className="w-1/2 text-right mt-2 py-6">
             <li className="font-bold text-gray-500">
-              <a href="">About</a>
+            <Link href="/"><a href="">About</a> </Link>
             </li>
           </ul>
         </div>
