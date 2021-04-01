f9878b4a22694747c7b6451ac7e4ec33d9a5b24a
karemarsy <karemarsy@gmail.com>
2021-04-01 08:32:11 +0200

    Create about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -863,5 +863,7 @@ module.exports = {
     wordBreak: ['responsive'],
     zIndex: ['responsive', 'focus-within', 'focus'],
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
