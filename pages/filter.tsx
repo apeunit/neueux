e7fee38b4abc834f0dfb7594fb5e4001d986a142
musayann <musayann@octangrp.com>
2021-05-06 21:31:05 +0200

    Adjust filter grid

--- pages/filter.tsx
+++ pages/filter.tsx
@@ -59,7 +59,7 @@ const FilterPage = ({ screens, filter }) => {
                 key={`screen-card-view-${screen.id}`}
                 className={[
                   screen.device === "desktop"
-                    ? "w-3/6"
+                    ? "w-full lg:w-3/6"
                     : "lg:w-1/6 md:w-1/3 sm:w-1/6 w-1/2",
                   "px-2 mt-5",
                 ].join(" ")}
