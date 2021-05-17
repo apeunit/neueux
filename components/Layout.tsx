1dbc11728aadae468b07809ec106c7abf9773d70
musayann <musayann@octangrp.com>
2021-05-17 13:18:16 +0200

    Upate the og:title strucuture

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,49 +23,52 @@ const Layout = ({
   backButton = false,
   editable = false,
   fill = false,
-}: Props) => (
-  <div className="overflow-hidden">
-    <Head>
-      <title>
-        {title ? `${title} | ` : null}
-        {Config.site_title}
-      </title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image || '/favicons/android-chrome-512x512.png'} />
-      <meta name="description" content={description} />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
-    </Head>
-    {!fill && (
-      <header>
-        <Navbar />
-      </header>
-    )}
-    {backButton && <BackButton />}
-    {children}
-    {!fill && <Footer editable={editable} />}
-  </div>
-);
+}: Props) => {
+  const siteTitle = title ? `${title} | ${Config.site_title}` : Config.site_title;
+  return (
+    <div className="overflow-hidden">
+      <Head>
+        <title>{siteTitle}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content={image || "/favicons/android-chrome-512x512.png"}
+        />
+        <meta name="description" content={description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      {!fill && (
+        <header>
+          <Navbar />
+        </header>
+      )}
+      {backButton && <BackButton />}
+      {children}
+      {!fill && <Footer editable={editable} />}
+    </div>
+  );
+};
 
 export default Layout;
