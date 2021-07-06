cb52144674a2f20ed6101e3f2a47efbd45d9b9ed
karemarsy <karema@octangrp.com>
2021-07-06 12:25:08 +0200

    fix: fix footer responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ import Button from "components/Button";
 const Footer = ({ editable }) => {
   const cms = useCMS();
   return (
-    <footer className="w-11/12 mx-auto border-t pt-4 pb-6 mt-40 border-gray-200">
+    <footer className="w-11/12 mx-auto border-t pt-4 pb-6 mt-24 border-gray-200">
       <div className="lg:flex md:flex justify-between text-gray-500 p-6 pb-24 lg:pb-0 md:pb-0">
-        <div className="lg:w-1/3 w-full text-sm">
+        <div className="lg:w-1/3 sm:w-1/3 md:w-1/3 w-full text-sm">
           <p>
             We are a network of designers, researchers and developers initiated
             by <a className="cursor-pointer underline">Ape Unit.</a> If this is interesting to you, please <a className="cursor-pointer underline">get in touch.</a>
@@ -17,12 +17,12 @@ const Footer = ({ editable }) => {
             <Link href="/imprint">Imprint</Link>
           </p>
         </div>
-        <div className="text-center lg:w-1/3 w-full pb-10 lg:pb-0 md:pb-0 ">
+        <div className="text-center lg:w-1/3 sm:w-1/3 md:w-1/3 w-full pb-10 lg:pb-0 md:pb-0 ">
           <span className="text-4xl">👹</span>
           <p className="text-gray-500 leading-loose text-sm pt-7">Supported by <a className="cursor-pointer underline">MolochDAO.</a></p>
         </div>
         {editable && (
-          <div className="lg:w-1/3 w-full lg:text-right">
+          <div className="lg:w-1/3 sm:w-1/3 md:w-1/3 w-full lg:text-right sm:text-right md:text-right hidden lg:block md:block sm:block">
             <Button onClick={() => cms.toggle()} size="lg">
               {cms.enabled ? "Exit edit mode" : "Edit this site"}
             </Button>
