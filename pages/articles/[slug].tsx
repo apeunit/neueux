b76e385c919191e8e7ae3ac20fd54e6e777b24ca
musayann <musayann@octangrp.com>
2021-08-09 12:19:36 +0200

    chore: fix serializing undefined preview prop

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -70,7 +70,7 @@ export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
     props: {
       article,
       slug,
-      preview,
+      preview: preview || false,
     },
   };
 };
