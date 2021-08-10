7a8c31963fedcbdec933b71169765502646eb233
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2021-08-10 13:40:35 +0200

    Update [slug].tsx

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -51,7 +51,7 @@ const App = ({ article, preview, slug }) => {
             </div>
             <p className="font-light text-gray-500 pt-5">{article.date}</p>
           </div>
-        <ReactMarkdown className="prose-xl lg:prose-2xl">{article.content}</ReactMarkdown>
+        <ReactMarkdown className="prose-lg lg:prose-xl">{article.content}</ReactMarkdown>
         </div>
       </main>
     </Layout>
