9a7950f0387facb642205cb8dc9454667135aeba
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2021-08-12 16:30:14 +0200

    starting to fix the spacing of the navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -13,9 +13,9 @@ const Navbar = () => {
   return (
     <>
     <nav className="">
-      <div className="w-11/12 justify-between mx-auto border-b border-gray-200">
-        <div className="flex text-base">
-          <ul className="w-full sm:w-1/2 py-6">
+      <div className="w-11/12 mx-auto border-b border-gray-200">
+        <div className="flex text-base py-6">
+          <ul className="w-full sm:w-1/2">
             <li className="cursor-pointer">
               <Link href="/">
                 <a>
@@ -25,25 +25,25 @@ const Navbar = () => {
             </li>
           </ul>
 
-          <ul className="w-32 text-center hidden lg:block sm:block md:block mt-2 py-6">
+          <ul className="w-32 text-center hidden lg:block sm:block md:block">
             <li className="font-normal text-gray-500 text-sm">
               <ActiveLink activeClassName="active" href="/"><a href="">Screens</a></ActiveLink>
             </li>
           </ul>
 
-          <ul className="w-32 text-center hidden lg:block sm:block md:block mt-2 py-6">
+          <ul className="w-32 text-center hidden lg:block sm:block md:block">
             <li className="font-normal text-gray-500 text-sm">
               <ActiveLink activeClassName="active" href="/articles"><a href="">Articles</a></ActiveLink>
             </li>
           </ul>
 
-          <ul className="w-1/2 text-right hidden lg:block sm:block md:block mt-2 py-6">
+          <ul className="w-1/2 text-right hidden lg:block sm:block md:block">
             <li className="font-bold text-gray-500 text-sm">
-              <Link href="/about"><a href="">About this project</a></Link>
+              <ActiveLink href="/about"><a href="">About this project</a></Link>
             </li>
           </ul>
 
-          <ul className="self-end text-right lg:hidden sm:hidden md:hidden mt-2 py-6">
+          <ul className="self-end text-right lg:hidden sm:hidden md:hidden">
             <li className="font-bold text-gray-500 text-sm">
             {/* <Button onClick={() => setShowFilter(true)} type="Primary" size="sm">
             Filter
