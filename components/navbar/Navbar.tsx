06dd6ecc3b7de6b16ed6dc089fc879fc47683174
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2021-08-13 10:32:51 +0200

    Update Navbar.tsx

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -33,8 +33,8 @@ const Navbar = () => {
           </ul>
 
           <ul className="w-32 items-end text-center hidden sm:block">
-            <li className="text-gray-500 text-sm">
-              <ActiveLink activeClassName="active" href="/articles" as="/articles"><a href="">Screens</a></ActiveLink>
+            <li className={router.pathname.split("?")[0] == "/articles" ? "active" : ""}>
+              <Link href="/articles">Articles</Link>
             </li>
           </ul>
           <ul className="w-1/2 items-end text-right hidden sm:block">
