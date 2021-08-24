4063a6a9d820ef6dbc7134e91f256082909a4414
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2021-08-24 17:16:45 +0200

    Update index.tsx

--- components/filter/index.tsx
+++ components/filter/index.tsx
@@ -146,7 +146,7 @@ const Filter = ({
         ))}
       </div>
 
-      <div className="hidden sm:inline">
+      <div className="hidden sm:inline self-center">
         <Button onClick={() => setShowFilter(true)} type="Primary" size="lg" >
           Filter by Categories
         </Button>
